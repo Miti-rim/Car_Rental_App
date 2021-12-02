05a9f915c8d79a07df01b258b50ed7dcc663fec2
Miti-rim <63088794+Miti-rim@users.noreply.github.com>
2021-12-02 19:01:03 +0600

    Add files via upload

---
+++ CarRentalApp/ResetPassword.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace CarRentalApp
+{
+    partial class ResetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbConfirmPass = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter New Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Confirm Password";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(61, 43);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(174, 20);
+            this.tbPass.TabIndex = 2;
+            // 
+            // tbConfirmPass
+            // 
+            this.tbConfirmPass.Location = new System.Drawing.Point(61, 133);
+            this.tbConfirmPass.Name = "tbConfirmPass";
+            this.tbConfirmPass.PasswordChar = '*';
+            this.tbConfirmPass.Size = new System.Drawing.Size(174, 20);
+            this.tbConfirmPass.TabIndex = 3;
+            this.tbConfirmPass.TextChanged += new System.EventHandler(this.tbConfirmPass_TextChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(90, 202);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(117, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset Password";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // ResetPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 278);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tbConfirmPass);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ResetPassword";
+            this.Text = "Reset Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.TextBox tbConfirmPass;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ CarRentalApp/ResetPassword.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalApp
+{
+    public partial class ResetPassword : Form
+    {
+        private readonly CarRentalDbEntities _db;
+        private User _user;
+        public ResetPassword(User user)
+        {
+            InitializeComponent();
+            _db = new CarRentalDbEntities();
+            _user = user;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var password = tbPass.Text;
+                var confirm_password = tbConfirmPass.Text;
+                var user = _db.Users.FirstOrDefault(q => q.id == _user.id);
+            if (password != confirm_password)
+                {
+                    MessageBox.Show("Password do not match. Please try again!");
+
+                }
+
+                _user.password = Utils.HashPassword(password);
+                _db.SaveChanges();
+
+                MessageBox.Show("Password was reset successfully");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("An Error has occured. Please try again");
+                //throw;
+            }
+            
+
+        }
+
+        private void tbConfirmPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
